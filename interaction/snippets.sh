22b0d838ccf2a967842ae6baac9ee85a0a1ea285
Atomiikgreg <gregory.berard11@gmail.com>
2022-06-22 21:46:25 +0200

    deploy devnet

--- interaction/snippets.sh
+++ interaction/snippets.sh
@@ -1,12 +1,22 @@
-ALICE="" # PEM path
+WALLET=wallet-owner.pem # PEM path
 ADDRESS=$(erdpy data load --key=address-devnet)
 DEPLOY_TRANSACTION=$(erdpy data load --key=deployTransaction-devnet)
 PROXY=https://devnet-gateway.elrond.com
 CHAIN_ID=D
 
+#string to hexa=0x$(xxd -pu <<< "arguments")
+#integer to hexa=0x$(printf '%x\n' arguments)
+
+# source snippets.sh && deploy
+
 deploy() {
-    erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${ALICE} \
+    local AMOUNT_OF_TOKENS=0x$(printf '%x\n' 10)
+    local ROYALTIES=0x$(printf '%x\n' 5)
+    local SELLING_PRICE=0x$(printf '%x\n' 1)
+
+    erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${WALLET} \
     --gas-limit=100000000 \
+    --arguments ${AMOUNT_OF_TOKENS} ${ROYALTIES} ${SELLING_PRICE} \
     --send --outfile="deploy-devnet.interaction.json" --proxy=${PROXY} --chain=${CHAIN_ID} || return
 
     TRANSACTION=$(erdpy data parse --file="deploy-devnet.interaction.json" --expression="data['emittedTransactionHash']")
@@ -23,14 +33,14 @@ issueNft() {
     local TOKEN_DISPLAY_NAME=0x4d79546573744e667464  # "MyTestNft"
     local TOKEN_TICKER=0x544553544e4654  # "TESTNFT"
 
-    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} \
+    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${WALLET.PEM} \
     --gas-limit=100000000 --value=50000000000000000 --function="issueToken" \
     --arguments ${TOKEN_DISPLAY_NAME} ${TOKEN_TICKER} \
     --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
 
 setLocalRoles() {
-    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} \
+    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${WALLET.PEM} \
     --gas-limit=100000000 --function="setLocalRoles" \
     --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
@@ -41,7 +51,7 @@ createNft() {
     local URI=0x72616e647572692e636f6d # randuri.com
     local SELLING_PRICE=0
 
-    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} \
+    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${WALLET.PEM} \
     --gas-limit=50000000 --function="createNft" \
     --arguments ${TOKEN_NAME} ${ROYALTIES} ${URI} ${SELLING_PRICE} \
     --send --proxy=${PROXY} --chain=${CHAIN_ID}
@@ -50,7 +60,7 @@ createNft() {
 buyNft() {
     local NFT_NONCE=1
 
-    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} \
+    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${WALLET.PEM} \
     --gas-limit=10000000 --function="buyNft" \
     --arguments ${NFT_NONCE} \
     --send --proxy=${PROXY} --chain=${CHAIN_ID}
